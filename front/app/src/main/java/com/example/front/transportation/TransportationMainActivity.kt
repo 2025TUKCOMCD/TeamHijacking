beab972ca01d45f3c5127abb4a25f04969a7a2dc
Chanho Yeon <yeonchanho0927@naver.com>
2025-01-09 09:45:42 +0900

    TransportationMainActivity 업데이트

--- front/app/src/main/java/com/example/front/transportation/TransportationMainActivity.kt
+++ front/app/src/main/java/com/example/front/transportation/TransportationMainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.front.transportation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.front.R
+import com.example.front.data.RouteProcessor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class TransportationMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_transportation_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        lifecycleScope.launch {
+            try {
+                val result = RouteProcessor.fetchAndProcessRoutes(
+                    startLat = 37.513841, // 잠실역
+                    startLng = 127.101823,
+                    endLat = 37.476813, // 낙성대역
+                    endLng = 126.964156
+                )
+                Log.d("TransportationMainActivity", "Received Data from RouteProcessor: $result")
+            } catch (e: Exception) {
+                Log.e("TransportationMainActivity", "Error fetching routes", e)
+            }
         }
     }
 }
