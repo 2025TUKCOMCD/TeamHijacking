3fefa22e3e482b56d922b9d1d004c301f55e9c71
Chanho Yeon <yeonchanho0927@naver.com>
2025-01-09 14:20:53 +0900

    대중교통 중요도 추가

--- front/app/src/main/java/com/example/front/transportation/TransportationMainActivity.kt
+++ front/app/src/main/java/com/example/front/transportation/TransportationMainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.front.transportation
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.front.R
+import com.example.front.data.RouteProcessor
+import com.example.front.databinding.ActivityTransportationMainBinding
+import kotlinx.coroutines.launch
 
 class TransportationMainActivity : AppCompatActivity() {
 
@@ -16,6 +22,21 @@ class TransportationMainActivity : AppCompatActivity() {
         binding = ActivityTransportationMainBinding.inflate(layoutInflater)
         setContentView(binding.main)
 
+        val tvRouteInfo: TextView = binding.tvRouteInfo
 
+
+        lifecycleScope.launch {
+            try {
+                val result = RouteProcessor.fetchAndProcessRoutes(
+                    startLat = 37.513841, // 잠실역
+                    startLng = 127.101823,
+                    endLat = 37.476813, // 낙성대역
+                    endLng = 126.964156
+                )
+                tvRouteInfo.text = result
+            } catch (e: Exception) {
+                Log.e("TransportationMainActivity", "Error fetching routes", e)
+            }
+        }
     }
 }
