a3187c67238f406fec9018d66c3dcad99fe7411b
srnb1 <hanisky1@naver.com>
2025-01-13 14:13:52 +0900

    [dev] AudioGuideActivity에 button layout 추가

--- front/app/src/main/java/com/example/front/audioguide/AudioGuideActivity.kt
+++ front/app/src/main/java/com/example/front/audioguide/AudioGuideActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.front.R
+import com.example.front.databinding.ActivityAudioGuideBinding
 
 class AudioGuideActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAudioGuideBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_audio_guide)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityAudioGuideBinding.inflate(layoutInflater)
+
+        setContentView(binding.main)
     }
 }
