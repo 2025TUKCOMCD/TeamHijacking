be180c19bfdb67848f0674fce0a5985bea585193
HyunBean0903 <hhb0664@naver.com>
2025-01-13 10:48:34 +0900

    출발지 및 도착지 둘다 나올 수 있게 바꾸고 UI/UX 살짝 만짐

--- front/app/src/main/java/com/example/front/transportation/TransportationNewPathActivity.kt
+++ front/app/src/main/java/com/example/front/transportation/TransportationNewPathActivity.kt
@@ -29,7 +29,8 @@ class TransportationNewPathActivity : AppCompatActivity() {
     companion object {
         private const val PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 100
         private const val PERMISSION_REQUEST_RECORD_AUDIO = 101
-        private const val REQUEST_CODE_SPEECH_INPUT = 102
+        private const val REQUEST_CODE_SPEECH_INPUT_START = 102
+        private const val REQUEST_CODE_SPEECH_INPUT_END = 103
         private const val GEOCODING_API_KEY = BuildConfig.Geolocation_APIKEY // 실제 API 키로 변경
         private const val TAG = "TransportationNewPathActivity"
     }
@@ -39,63 +40,81 @@ class TransportationNewPathActivity : AppCompatActivity() {
         binding = ActivityTransportationNewPathBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val buttonStartSTT: Button = binding.buttonStartStt
-        val addressTextView: TextView = binding.addressTextView
-        val latitudeTextView: TextView = binding.latitudeTextView
-        val longitudeTextView: TextView = binding.longitudeTextView
+        //사용할 객체들 선언
+        val buttonStartSTTStart: Button = binding.buttonStartSttStart
+        val buttonStartSTTEnd: Button = binding.buttonStartSttEnd
+        val addressStartTextView: TextView = binding.addressStartTextView
+        val addressEndTextView: TextView = binding.addressEndTextView
+        val latitudeStartTextView: TextView = binding.latitudeStartTextView
+        val longitudeStartTextView: TextView = binding.longitudeStartTextView
+        val latitudeEndTextView: TextView = binding.latitudeEndTextView
+        val longitudeEndTextView: TextView = binding.longitudeEndTextView
 
+        // 권한이 있는지 확인 (여기서는 위치 권한 확인)
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             val address = "서울특별시 강남구 강남대로 323"
-            addressTextView.text = "Address: $address"
-            getLocationFromAddress(address, latitudeTextView, longitudeTextView)
-        } else {
+            addressStartTextView.text = "Start Address: $address"
+            getLocationFromAddress(address, latitudeStartTextView, longitudeStartTextView)
+        } else { // 만약 권한이 없으면 권한 요청
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
         }
 
+        // 오디오 권한 요청
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST_RECORD_AUDIO)
         }
-
-        buttonStartSTT.setOnClickListener {
-            startSTT()
+        //시작지점 STT함수
+        buttonStartSTTStart.setOnClickListener {
+            startSTT(REQUEST_CODE_SPEECH_INPUT_START)
+        }
+        //도착지점 STT함수
+        buttonStartSTTEnd.setOnClickListener {
+            startSTT(REQUEST_CODE_SPEECH_INPUT_END)
         }
     }
 
-    private fun startSTT() {
+    private fun startSTT(requestCode: Int) {
+        // 음성 인식을 위한 인텐트
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        // 음성 인식 모델 설정
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak the address...")
-
+        // 음성 인식을 위한 기본 언어: 한국어
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREA)
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, if (requestCode == REQUEST_CODE_SPEECH_INPUT_START) "Speak the start address..." else "Speak the end address...")
         try {
-            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            startActivityForResult(intent, requestCode)
         } catch (e: ActivityNotFoundException) {
             e.printStackTrace()
         }
     }
-
+    //STT이후 오게되는 함수 만약 REQUESTCODE ==REQUET_CODE_INPUT_START라면 STARTADRESS를 바꿔주고 아니면 END ADDRESS를 바꿔준다
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK && data != null) {
+        if ((requestCode == REQUEST_CODE_SPEECH_INPUT_START || requestCode == REQUEST_CODE_SPEECH_INPUT_END) && resultCode == RESULT_OK && data != null) {
             val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
             if (result != null && result.isNotEmpty()) {
                 val address = result[0]
-                binding.addressTextView.text = "Address: $address"
-                getLocationFromAddress(address, binding.latitudeTextView, binding.longitudeTextView)
+                if (requestCode == REQUEST_CODE_SPEECH_INPUT_START) {
+                    binding.addressStartTextView.text = "Start Address: $address"
+                    getLocationFromAddress(address, binding.latitudeStartTextView, binding.longitudeStartTextView)
+                } else {
+                    binding.addressEndTextView.text = "End Address: $address"
+                    getLocationFromAddress(address, binding.latitudeEndTextView, binding.longitudeEndTextView)
+                }
             }
         }
     }
 
     private fun getLocationFromAddress(address: String, latitudeTextView: TextView, longitudeTextView: TextView) {
         Log.d(TAG, "getLocationFromAddress: Starting geocoding for address $address")
-
+        // HTTP 요청 형식: API 키와 주소를 입력하면 위도 경도로 바꿔줌
         val client = OkHttpClient()
         val url = "https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=$GEOCODING_API_KEY"
 
         val request = Request.Builder()
             .url(url)
             .build()
-
+        //실패시 출력
         client.newCall(request).enqueue(object : okhttp3.Callback {
             override fun onFailure(call: okhttp3.Call, e: IOException) {
                 Log.e(TAG, "Geocoding API call failed", e)
@@ -104,7 +123,7 @@ class TransportationNewPathActivity : AppCompatActivity() {
                     longitudeTextView.text = "Longitude: Error"
                 }
             }
-
+            //성공시 함수
             override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
                 if (response.isSuccessful) {
                     val responseBody = response.body?.string()
@@ -112,12 +131,12 @@ class TransportationNewPathActivity : AppCompatActivity() {
 
                     val json = JsonParser.parseString(responseBody) as JsonObject
                     val results = json["results"].asJsonArray
-
+                    //대충 JSON추출 하는 코드라는 뜻
                     if (results.size() > 0) {
                         val location = results[0].asJsonObject["geometry"].asJsonObject["location"].asJsonObject
                         val latitude = location["lat"].asDouble
                         val longitude = location["lng"].asDouble
-
+                        //텍스트를 넣어줌
                         runOnUiThread {
                             latitudeTextView.text = "Latitude: $latitude"
                             longitudeTextView.text = "Longitude: $longitude"
@@ -139,14 +158,14 @@ class TransportationNewPathActivity : AppCompatActivity() {
             }
         })
     }
-
+    //Default함수 일단 실행 먼저 됨 API와의 통신을 위함
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISSION_REQUEST_ACCESS_FINE_LOCATION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             val address = "서울특별시 강남구 강남대로 323"
-            val latitudeTextView: TextView = binding.latitudeTextView
-            val longitudeTextView: TextView = binding.longitudeTextView
-            getLocationFromAddress(address, latitudeTextView, longitudeTextView)
+            val latitudeStartTextView: TextView = binding.latitudeStartTextView
+            val longitudeStartTextView: TextView = binding.longitudeStartTextView
+            getLocationFromAddress(address, latitudeStartTextView, longitudeStartTextView)
         }
     }
 }
