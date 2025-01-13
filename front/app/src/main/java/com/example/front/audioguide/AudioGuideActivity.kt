e35e984b6ab0380b2340425d1174bbb348bc251e
srnb1 <hanisky1@naver.com>
2025-01-13 15:30:19 +0900

    [dev] TransportationMainActivity 디자인 변경

--- front/app/src/main/java/com/example/front/audioguide/AudioGuideActivity.kt
+++ front/app/src/main/java/com/example/front/audioguide/AudioGuideActivity.kt
@@ -1,12 +1,9 @@
 package com.example.front.audioguide
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.front.R
 import com.example.front.databinding.ActivityAudioGuideBinding
 
 class AudioGuideActivity : AppCompatActivity() {
@@ -14,8 +11,8 @@ class AudioGuideActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAudioGuideBinding
 
     //XML에서 정의된 버튼들을 연결
-    val blinkerBtn1: ImageButton = binding.blinkerBtn1
-    val blinkerBtn2: ImageButton = binding.blinkerBtn2
+    private val blinkerBtn1: ImageButton = binding.blinkerBtn1
+    private val blinkerBtn2: ImageButton = binding.blinkerBtn2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +22,13 @@ class AudioGuideActivity : AppCompatActivity() {
 
         //음성 유도기, 횡단보도 유도 기능
         blinkerBtn1.setOnClickListener{
+            Log.d("Blinker", "시각장애인용 음향 유도기 안내 버튼 클릭")
 
         }
 
         //음성 신호기 기능
         blinkerBtn2.setOnClickListener{
-
+            Log.d("Blinker", "시각장애인용 음향 신호기 작동 버튼 클릭")
         }
 
     }
