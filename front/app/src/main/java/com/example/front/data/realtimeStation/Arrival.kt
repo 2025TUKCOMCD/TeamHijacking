d7ec8f5257509e18f57db079d0cb54a876b2f2fd
Chanho Yeon <yeonchanho0927@naver.com>
2025-01-15 15:16:45 +0900

    저장

--- front/app/src/main/java/com/example/front/data/realtimeStation/Arrival.kt
+++ front/app/src/main/java/com/example/front/data/realtimeStation/Arrival.kt
@@ -1,3 +1,15 @@
 package com.example.front.data.realtimeStation
 
-data class Arrival()
+data class Arrival(
+    val fulCarAt: String,
+    val busPlateNo: String,
+    val waitStatus: String,
+    val busStatus: String,
+    val endBusYn: String,
+    val leftStation: Int,
+    val lowBusYn: String,
+    val congestion: Int,
+    val arrivalSec: Int,
+    val nmprType: Int
+)
+
--- front/app/src/main/java/com/example/front/data/realtimeStation/RealtimeStation.kt
+++ front/app/src/main/java/com/example/front/data/realtimeStation/RealtimeStation.kt
@@ -7,15 +7,3 @@ data class RealtimeStation(
 
 
 
-data class Arrival(
-    val fulCarAt: String,
-    val busPlateNo: String,
-    val waitStatus: String,
-    val busStatus: String,
-    val endBusYn: String,
-    val leftStation: Int,
-    val lowBusYn: String,
-    val congestion: Int,
-    val arrivalSec: Int,
-    val nmprType: Int
-)
