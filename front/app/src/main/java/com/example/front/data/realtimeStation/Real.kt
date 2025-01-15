1f40c5de887841d521e89e4a47c6bc20e6c74077
Chanho Yeon <yeonchanho0927@naver.com>
2025-01-15 15:16:53 +0900

    저장

--- front/app/src/main/java/com/example/front/data/realtimeStation/Real.kt
+++ front/app/src/main/java/com/example/front/data/realtimeStation/Real.kt
@@ -1,3 +1,11 @@
 package com.example.front.data.realtimeStation
 
-data class Real()
+data class Real(
+    val routeId: String,
+    val updownFlag: String,
+    val arrival1: Arrival?,
+    val arrival2: Arrival?,
+    val localRouteId: String,
+    val stationSeq: String,
+    val routeNm: String
+)
