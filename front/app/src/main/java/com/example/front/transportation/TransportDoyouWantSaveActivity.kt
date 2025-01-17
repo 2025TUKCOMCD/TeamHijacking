922ebb679d3c81b7bdfc428b4ce966e827a76820
srnb1 <hanisky1@naver.com>
2025-01-17 13:34:02 +0900

    [Dev] 경로 저장용 dialog 추가를 위한 activity 생성

---
+++ front/app/src/main/java/com/example/front/transportation/TransportDoyouWantSaveActivity.kt
@@ -0,0 +1,21 @@
+package com.example.front.transportation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.front.R
+
+class TransportDoyouWantSaveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_transport_doyou_want_save)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
