e18395f9165031c30470592a871699b4549e1ba9
srnb1 <hanisky1@naver.com>
2025-01-17 13:43:38 +0900

    [Dev] 교통 안내용 Activity 추가

---
+++ front/app/src/main/java/com/example/front/transportation/TransportInfrmationActivity.kt
@@ -0,0 +1,22 @@
+package com.example.front.transportation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.front.R
+
+//교통 안내용 Activity
+class TransportInfrmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_transport_infrmation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
