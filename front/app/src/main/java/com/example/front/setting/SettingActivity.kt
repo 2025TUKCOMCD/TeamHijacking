d0834c079b4da545475476083da7389e21b3c59c
srnb1 <hanisky1@naver.com>
2025-01-27 13:26:16 +0900

    [UIUX] SettingView에 이것저것 넣는 중

--- front/app/src/main/java/com/example/front/setting/SettingActivity.kt
+++ front/app/src/main/java/com/example/front/setting/SettingActivity.kt
@@ -1,21 +1,19 @@
 package com.example.front.setting
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.front.R
+import com.example.front.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_setting)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //view 바인딩
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
