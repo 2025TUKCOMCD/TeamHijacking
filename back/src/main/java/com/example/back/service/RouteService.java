fa71839708d062339141bec50679531eed9d40f7
Chanho Yeon <yeonchanho0927@naver.com>
2025-02-03 17:43:08 +0900

    서버 기본 설정

--- back/src/main/java/com/example/back/service/RouteService.java
+++ back/src/main/java/com/example/back/service/RouteService.java
@@ -1,7 +1,5 @@
 package com.example.back.service;
 
 public class RouteService {
-        List<PathRouteResult> fetchAndProcessRoutes(double startLat, double startLng, double endLat, double endLng);
-        List<Map<String, String>> fetchBusRouteDetails(int busID, int startStationID, int endStationID, String startLocalStationID, String endLocalStationID);
-        double calculateRouteScore(Path path)
+
 }
--- back/src/main/java/com/example/back/service/impl/RouteServiceImpl.java
+++ back/src/main/java/com/example/back/service/impl/RouteServiceImpl.java
@@ -1,18 +1,4 @@
 package com.example.back.service.impl;
 
 public class RouteServiceImpl {
-    private static final String BASE_URL = "https://api.odsay.com/v1/api/";
-    private static final String ODsay_APIKEY = BuildConfig.ODsay_APIKEY;
-    private final Gson gson = new Gson();
-    private final OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS)
-            .build();
-
-    private final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
 }
