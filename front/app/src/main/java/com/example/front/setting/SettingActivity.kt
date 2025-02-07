4b9fbd308ada6360bc8132df86915a215fa71a47
srnb1 <hanisky1@naver.com>
2025-02-07 14:36:59 +0900

    [UIUX] SettingActivity를 위한 default dialog 추가

--- front/app/src/main/java/com/example/front/setting/SettingActivity.kt
+++ front/app/src/main/java/com/example/front/setting/SettingActivity.kt
@@ -1,8 +1,10 @@
 package com.example.front.setting
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.front.databinding.ActivitySettingBinding
+import org.w3c.dom.Text
 
 class SettingActivity : AppCompatActivity() {
 
@@ -15,5 +17,26 @@ class SettingActivity : AppCompatActivity() {
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //각각의 객체 바인딩
+        val settingProfile: TextView = binding.settingProfile
+        val settingNewestVersion: TextView = binding.settingNewestVersion
+        val settingFAQ: TextView = binding.settingFAQ
+        val askFormBtt: TextView = binding.askFormBtt
+
+        //클릭 시 이동하도록...
+        settingProfile.setOnClickListener{
+
+        }
+        settingNewestVersion.setOnClickListener{
+
+        }
+        settingFAQ.setOnClickListener {
+
+        }
+        askFormBtt.setOnClickListener {
+
+        }
     }
+
+
 }
