a2b99a30a4a9330fa9c4832a356d4dd5c79e0f96
srnb1 <hanisky1@naver.com>
2025-02-14 14:38:37 +0900

    [UIUX] KAKAO LOGIN BTN 추가

--- front/android2/src/main/java/com/example/front/Login/LoginActivity.kt
+++ front/android2/src/main/java/com/example/front/Login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.front.Login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.front.databinding.ActivityLoginBinding
@@ -12,14 +13,26 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var loginButton = binding.loginButton
+
+        val loginButton: ImageButton = binding.loginButton
+
         loginButton.setOnClickListener {
+
+            //TODO:: 로그인이 끝난 후 실행될 수 있도록 조정할 것
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
 
+    //추후 구현할, 로그인 function
+    fun kakaoLogin(){
+
+    }
+
+    //TODO:: 로그인 여부를 자동 확인하는 func 구현 필요
+    fun isUserLogin(){
+
+    }
 }
