d168d775cc2b955196bb1d6e268d301ddf128065
HyunBean0903 <hhb0664@naver.com>
2025-03-26 14:53:01 +0900

    사용자 위치 얻기 "locationManager 할당 완료 후 테스팅 실패"

--- front/app/src/main/java/com/example/front/transportation/DetailRouteFragment/BusFragment.kt
+++ front/app/src/main/java/com/example/front/transportation/DetailRouteFragment/BusFragment.kt
@@ -1,6 +1,9 @@
 package com.example.front.transportation.DetailRouteFragment
 
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -16,6 +19,7 @@ import com.example.front.transportation.FragmentNavigation
 class BusFragment : Fragment() {
 
     private var navigation: FragmentNavigation? = null
+    private lateinit var locationManager: LocationManager
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -34,12 +38,18 @@ class BusFragment : Fragment() {
 
         val busNumber = arguments?.getString("busNumber") ?: ""
         val busStation = arguments?.getString("busStation") ?: ""
-
+        val locationBtn =view.findViewById<AppCompatImageButton>(R.id.locationButton)
         view.findViewById<TextView>(R.id.busNumberTextView).text = "버스 번호: $busNumber"
         view.findViewById<TextView>(R.id.busStationTextView).text = "버스 정류장: $busStation"
         Log.d("현빈", "일단 테스트123")
+        locationManager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
+        Log.d("현빈", "데이터 들어감")
+        locationBtn.setOnClickListener {
+            Log.d("현빈", "클릭함")
+            getLocation()
+        }
 
-        val nextButton = view.findViewById<AppCompatImageButton>(R.id.nextButton) // AppCompatImageButton으로 캐스팅
+        val nextButton = view.findViewById<AppCompatImageButton>(R.id.nextButton)
         nextButton.setOnClickListener {
             navigation?.showNextFragment()
         }
@@ -50,5 +60,31 @@ class BusFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         navigation = null
+    }
+
+    private fun getLocation() {
+        Log.d("현빈", "함수 입성")
+        checkPermissions(requireActivity())
+        val location: Location? = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        Log.d("현빈", "설정 완료")
+        Log.d("현빈", location.toString())
+        val latitude = location?.latitude
+        val longitude = location?.longitude
+        val accuracy = location?.accuracy
+        val time = location?.time
+        Log.d("현빈", latitude.toString())
+        Log.d("현빈", longitude.toString())
+        Log.d("현빈", accuracy.toString())
+        Log.d("현빈", time.toString())
+        location?.let {
+            val latitude = it.latitude
+            val longitude = it.longitude
+            val accuracy = it.accuracy
+            val time = it.time
+            Log.d("현빈", latitude.toString())
+            Log.d("현빈", longitude.toString())
+            Log.d("현빈", accuracy.toString())
+            Log.d("현빈", time.toString())
+        }
     }
 }
