65b0a07884e802203f430c1dd1354265dd966c11
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-04-26 13:34:06 +0900

    user 레포지토리 리팩토링

--- front/android2/src/main/java/com/example/front/Login/processor/UserProcessor.kt
+++ front/android2/src/main/java/com/example/front/Login/processor/UserProcessor.kt
@@ -36,22 +36,37 @@ object UserProcessor {
     fun getUserByLogin(loginId: String, callback: (User?) -> Unit) {
         userService.getUserByLoginId(loginId).enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
-                callback(response.body())
+                if (response.isSuccessful) {
+                    Log.d("UserProcessor", "조회 성공: ${response.body()}")
+                    callback(response.body())
+                } else {
+                    Log.e("UserProcessor", "조회 실패 - 서버 오류: ${response.code()}")
+                    callback(null)
+                }
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.e("UserProcessor", "조회 실패 - 통신 오류: ${t.message}")
                 callback(null)
             }
         })
     }
 
+    //사용자 정보 수정
     fun updateUser(user: User, callback: (User?) -> Unit) {
         userService.updateUser(user.loginId, user).enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
-                callback(response.body())
+                if (response.isSuccessful) {
+                    Log.d("UserProcessor", "수정 성공: ${response.body()}")
+                    callback(response.body())
+                } else {
+                    Log.e("UserProcessor", "수정 실패 - 서버 오류: ${response.code()}")
+                    callback(null)
+                }
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.e("UserProcessor", "수정 실패 - 통신 오류: ${t.message}")
                 callback(null)
             }
         })
