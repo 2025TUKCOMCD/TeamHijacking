a22382571fc99bc46fdda39b7f3cd761948e8484
Chanho Yeon <yeonchanho0927@naver.com>
2025-04-30 12:30:09 +0900

    commit 수정

--- front/android2/src/main/java/com/example/front/Login/processor/RetrofitClient.kt
+++
@@ -1,9 +0,0 @@
-package com.example.front.login.processor
-
-object RetrofitClient {
-    /*Retrofit.Builder 이용 서버 기본 URL 설정
-    * JSON 데이터 자동 으로, 객체로 변환 - Converter
-    * 또, Retrofit.create() 이용해 Interface 구현체 생성*/
-
-    private const val BASE_URL =
-}
