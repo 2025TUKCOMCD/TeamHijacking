eb5a433228751087cb78d88ec7d554918f76dca4
Chanho Yeon <yeonchanho0927@naver.com>
2025-05-07 00:07:55 +0900

    경우에 따른 처리 기능 추가

---
+++ back/src/main/java/com/example/back/service/MessageService.java
@@ -0,0 +1,59 @@
+package com.example.back.service;
+
+
+import com.example.back.dto.RealTimeResultDTO;
+import com.example.back.dto.realtime.RealtimeDTO;
+
+import com.example.back.dto.subway.SubwayArriveProcessDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private APIService apiService;
+
+//    // 지하철 도착 정보
+//    protected  RealTimeResultDTO processSubwayBoardingData(RealtimeDTO data) throws IOException {
+//        List<SubwayArriveProcessDTO.RealtimeArrival> realtimeArrivals = null;
+//        realtimeArrivals = apiService.fetchAndSubwayArrive(data.getStationName()).getRealtimeArrivalList();
+//
+//    }
+//
+//    // 지하철 도착 정보 + 실시간 위치
+//    protected RealTimeResultDTO processSubwayAlightingData(RealtimeDTO data) throws IOException {
+//        // 지하철 실시간 위치 정보 가져오기
+//        List<SubwayArriveProcessDTO.RealtimeArrival> realtimeArrivals = apiService.fetchAndSubwayArrive(data.getStationName()).getRealtimeArrivalList();
+//
+//        // 지하철 도착 정보 가져오기
+//        List<SubwayArriveProcessDTO.RealtimeArrival> subwayArrivals = apiService.fetchAndSubwayArrive(data.getStationName()).getRealtimeArrivalList();
+//
+//
+//    }
+//
+//    // 버스 도착 정보
+//    protected RealTimeResultDTO processBusBoardingData(RealtimeDTO data) throws IOException{
+//
+//    }
+//
+//    // 버스 도착 정보 + 실시간 위치
+//    protected RealTimeResultDTO processBusAlightingData(RealtimeDTO data) throws IOException{
+//        busApi.getBusPosByRouteSt()
+//    }
+//
+//    // 도보의 경우 다음 대중교통 정보 요구 처리
+//    protected RealTimeResultDTO processWalkingData(RealtimeDTO data) throws IOException{
+//
+//    }
+//
+//    // 예외 처리
+//    protected RealTimeResultDTO processDefaultData(RealtimeDTO data){
+//
+//    }
+
+}
