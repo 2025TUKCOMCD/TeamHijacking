8d1df68384b439be6b9dcc5da5c7129efbb10ceb
Chanho Yeon <yeonchanho0927@naver.com>
2025-05-07 00:08:14 +0900

    Config 파일 삭제 및 수정

--- back/src/main/java/com/example/back/controller/RealtimeController.java
+++ back/src/main/java/com/example/back/controller/RealtimeController.java
@@ -1,6 +1,6 @@
 package com.example.back.controller;
 
-import com.example.back.config.RealtimeWebSocketHandler;
+import com.example.back.service.RealtimeWebSocketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -11,18 +11,18 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class RealtimeController implements WebSocketConfigurer {
 
-    private final RealtimeWebSocketHandler realtimeWebSocketHandler;
+    private final RealtimeWebSocketService realtimeWebSocketService;
 
     // RealtimeWebSocketHandler를 주입받는 생성자
     @Autowired
-    public RealtimeController(RealtimeWebSocketHandler realtimeWebSocketHandler) {
-        this.realtimeWebSocketHandler = realtimeWebSocketHandler;
+    public RealtimeController(RealtimeWebSocketService realtimeWebSocketService) {
+        this.realtimeWebSocketService = realtimeWebSocketService;
     }
 
     // WebSocketConfigurer 인터페이스의 메서드 구현
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(realtimeWebSocketHandler, "/realtime")
+        registry.addHandler(realtimeWebSocketService, "/realtime")
                 .setAllowedOrigins("*");
     }
 }
