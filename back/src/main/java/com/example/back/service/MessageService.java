ea8edc179c47792cf48fb36e47671b09c81caec7
Chanho Yeon <yeonchanho0927@naver.com>
2025-05-07 23:32:24 +0900

    MessageService 수정

--- back/src/main/java/com/example/back/service/MessageService.java
+++ back/src/main/java/com/example/back/service/MessageService.java
@@ -1,15 +1,8 @@
 package com.example.back.service;
 
 
-import com.example.back.dto.RealTimeResultDTO;
-import com.example.back.dto.realtime.RealtimeDTO;
-
-import com.example.back.dto.subway.SubwayArriveProcessDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.List;
 
 
 @Service
