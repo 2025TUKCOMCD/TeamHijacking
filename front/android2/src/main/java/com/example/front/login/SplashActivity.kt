4c374691b1303954e34b3f746b847a26b21d377b
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-15 18:33:09 +0900

    [UIUX] 스마트워치앱의 UIUX 수정

--- front/android2/src/main/java/com/example/front/login/SplashActivity.kt
+++ front/android2/src/main/java/com/example/front/login/SplashActivity.kt
@@ -2,17 +2,13 @@ package com.example.front.login
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.front.MainActivity
-import com.example.front.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// setContentView(R.layout.activity_splash) // 간단한 로고나 배경이 있는 레이아웃 띄울 수 있음
+        //setContentView(R.layout.activity_splash) // 간단한 로고나 배경이 있는 레이아웃 띄울 수 있음
 
         // 예: SharedPreferences 에 로그인 상태 저장 되어 있다고 가정
         val sharedPrefs = getSharedPreferences("userPrefs", MODE_PRIVATE)
