4454587ca668b410bb555b66db2353ffa963ffd8
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-17 14:51:32 +0900

    [UIUX] 뒤로가기 개선할 수 없는 건 아닌데 이대로도 괜찮겠다 싶어서 commit

--- front/android2/src/main/java/com/example/front/MainActivity.kt
+++ front/android2/src/main/java/com/example/front/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.front
 
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -14,6 +17,7 @@ import com.example.front.Iot.Iot_page02
 import com.example.front.Iot.Iot_page03
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import org.w3c.dom.Text
 
 // FragmentStateAdapter
 //Fragment 화면 이동 하게 해주는 코드
@@ -47,22 +51,49 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "어서오세요 $name 님", Toast.LENGTH_SHORT).show()
 
         //탭 레이아웃 으로 하단에 나오는 버튼 이름을 일단 설정
-        val tabLayout: TabLayout = binding.tabLayout //
+        val tabLayout: TabLayout = binding.tabLayout
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             when (position) {
                 0 -> {
-                    tab.setIcon(R.drawable.ic_home) // 홈 아이콘 설정
-                    tab.text = "홈" // 텍스트 (선택 사항)
+                    tab.setIcon(R.drawable.ic_home) // 아이콘 설정
+                    tab.text = "관리" // 텍스트
                 }
                 1 -> {
-                    tab.setIcon(R.drawable.ic_add) // + 아이콘 설정
-                    tab.text = "추가" // 텍스트 (선택 사항)
+                    tab.setIcon(R.drawable.ic_add)
+                    tab.text = "추가"
                 }
                 2 -> {
-                    tab.setIcon(R.drawable.ic_settings) // 제어 아이콘 설정
-                    tab.text = "제어" // 텍스트 (선택 사항)
+                    tab.setIcon(R.drawable.ic_settings)
+                    tab.text = "설정"
                 }
             }
         }.attach()
+
+        //ViewPager 의 페이지 변경에 따라 툴바 텍스트 변경
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                val title = when (position) {
+                    0 -> "IoT 기기 관리"
+                    1 -> "IoT 기기 추가"
+                    2 -> "설정"
+                    else -> "IoT 기기 관리"
+                }
+                binding.toolBarText.text = title
+                binding.backStepBtn.visibility = if (position == 0) android.view.View.GONE else android.view.View.VISIBLE
+            }
+        })
+
+        //뒤로 가기 진행
+        binding.backStepBtn.setOnClickListener {
+            val currentPosition = binding.viewpager.currentItem
+            Log.d("ViewPager", "현재 위치: $currentPosition")
+            if ( currentPosition > 0 ) {
+                val newPosition = currentPosition - 1
+                binding.viewpager.setCurrentItem(newPosition, true)
+                Log.d("ViewPager", "현재 위치: $currentPosition -> 이동할 위치: ${currentPosition - 1}")
+            }
+        }
     }
 }
