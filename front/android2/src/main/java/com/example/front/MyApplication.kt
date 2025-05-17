548c9841e94717c88ffe8e562628510e3f480161
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-17 11:34:55 +0900

    [UIUX] 스마트폰 앱의 bottomNavigationBar 디자인 수정

--- front/android2/src/main/java/com/example/front/MyApplication.kt
+++ front/android2/src/main/java/com/example/front/MyApplication.kt
@@ -5,7 +5,6 @@ import android.app.Application
 import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
-//import com.umc.anddeul.R
 
 /*https://uroa.tistory.com/43
    전역 변수? 를 공유 하기 위한 Application Class.
@@ -18,7 +17,7 @@ class MyApplication: Application() {
 
         val keyHash = Utility.getKeyHash(this)
         Log.d("키 확인", keyHash)
-        //카카오 SDK 초기화, 네이티브 앱 키를 사용해 SDK를 초기화
+        //카카오 SDK 초기화, native 앱 키를 사용해 SDK 초기화
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_API_KEY)
     }
 }
