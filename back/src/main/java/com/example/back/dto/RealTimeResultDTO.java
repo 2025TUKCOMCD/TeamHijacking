e0720dcafca4e1fa9ff383af549f56479896261e
Chanho Yeon <yeonchanho0927@naver.com>
2025-06-06 10:40:11 +0900

    에러 처리

--- back/src/main/java/com/example/back/dto/RealTimeResultDTO.java
+++ back/src/main/java/com/example/back/dto/RealTimeResultDTO.java
@@ -1,12 +1,18 @@
 package com.example.back.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
 public class RealTimeResultDTO {
+    @JsonInclude(value = JsonInclude.Include.NON_DEFAULT)
     private int trainNo; // 기차 번호
+    @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+    private String vehId;
     private int nextRequest; // 1: 현재 대중교통 2: 다음 대중교통
+    @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
     private String location; // 현재 위치
+
     private String predictTimes1;
     private String predictTimes2;
 }
