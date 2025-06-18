b81d7d5d2e1dba96b518c69358f4b49d624e4016
Chanho Yeon <yeonchanho0927@naver.com>
2025-06-18 17:25:11 +0900

    경로 저장 및 가져오기 함수 추가

--- front/app/src/main/java/com/example/front/transportation/service/RouteService.kt
+++ front/app/src/main/java/com/example/front/transportation/service/RouteService.kt
@@ -1,16 +1,28 @@
-import com.example.front.transportation.data.DB.DBSave
+import com.example.front.transportation.data.DB.DBSaveDTO
+import com.example.front.transportation.data.DB.GetRouteResponseDTO
+import com.example.front.transportation.data.DB.SavedRouteResponseDTO
 import com.example.front.transportation.data.searchPath.Route
 import com.example.front.transportation.data.searchPath.RouteRequest
+import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface RouteService {
     @POST("api/route")
     suspend fun getRoute(@Body request: RouteRequest): List<Route>
 
     @POST("api/DBSaveRoute")
-    suspend fun saveDBRoute(@Body request: DBSave): Boolean
+    suspend fun saveDBRoute(@Body request: DBSaveDTO): SavedRouteResponseDTO
 
-    @POST("api/DBDeleteRoute")
-    suspend fun deleteDBRoute(@Body request: RouteRequest): List<Route>
+    @POST("api/updateFavorite")
+    suspend fun updateFavorite(@Body request: DBSaveDTO): SavedRouteResponseDTO
+
+    @GET("api/DBGetRoute")
+    suspend fun getSavedRoute(@Query("loginId") loginId: String): List<GetRouteResponseDTO>
+
+//    @POST("api/DBDeleteRoute")
+//    suspend fun deleteDBRoute(@Body request: RouteRequest): List<Route>
+
 }
