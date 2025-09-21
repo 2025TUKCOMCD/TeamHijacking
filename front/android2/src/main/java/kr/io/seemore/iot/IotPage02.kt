5f2b42b46445489b8fcb02c2ec01d148258c7bbe
HyunBeen0903 <hhb0664@naver.com>
2025-09-21 17:04:01 +0900

    [feat] 호선 별 전화번호 기능 매핑 및 워치 핸드폰 간 데이터 송수인 파이프 라인 기능 활성화

--- front/android2/src/main/java/kr/io/seemore/iot/IotPage02.kt
+++ front/android2/src/main/java/kr/io/seemore/iot/IotPage02.kt
@@ -23,6 +23,7 @@ class  IotPage02 : Fragment() {
     private val binding get() = _binding!!
     private val tag = "IoT_page02"
     private lateinit var telToSubwayBtn: Button
+    private lateinit var call_info : Pair<Int, String>
 
     // 데이터 수신을 위한 BroadcastReceiver
     private val dataReceiver = object : BroadcastReceiver() {
@@ -36,6 +37,9 @@ class  IotPage02 : Fragment() {
                 val transitTypeNo: ArrayList<String>? = it.getStringArrayListExtra("transitTypeNo")
                 Log.d(tag, "Received path transit types: $transitTypeNo")
 
+                val departureName: String = it.getStringExtra("departureName").toString()
+                Log.d(tag, "Received path transit types: $departureName")
+
                 // pathTransitType 배열에 1이 포함되어 있는지 확인
                 val isConditionMet = pathTransitType?.contains(1) == true
 
@@ -43,8 +47,12 @@ class  IotPage02 : Fragment() {
                     telToSubwayBtn.visibility = View.VISIBLE
                     for(i in conditionMetindex){
                         val transitNum = transitTypeNo?.get(i)
-                        val filterdpathTransitType = transitNum?.get(4)
-                        Log.d(tag, "${filterdpathTransitType}")
+                        var filterdpathTransitType = transitNum?.get(4)!!.toString()
+                        val intTransitType = filterdpathTransitType.toInt()
+                        call_info = selectCallNum(intTransitType, departureName)
+
+                        Log.d(tag, "${call_info.first}호선 역무실 전화연결\n ${call_info.second}")
+                        binding.subwayPhone.text = "${call_info.first}호선 역무실 전화연결\n${call_info.second}"
                     }
 
                     Log.d(tag, "pathTransitType에 1이 포함되어 있어 버튼을 보이게 합니다.")
@@ -74,7 +82,7 @@ class  IotPage02 : Fragment() {
 //        telToSubwayBtn.visibility = View.GONE
 
         telToSubwayBtn.setOnClickListener {
-            callToSubway()
+            callToSubway(call_info.first, call_info.second)
         }
 
         return binding.root
@@ -88,8 +96,8 @@ class  IotPage02 : Fragment() {
     }
 
     //------------custom func 전화번호 선택 함수
-    fun callToSubway() {
-        var phoneNumber: String = selectCallNum() //selectCallNum
+    fun callToSubway(lineName : Int, callnums:String)  {
+        var phoneNumber: String = callnums
         //위치 정보 받아왔다면 여기에서 체크::
         //혹은 위치에 따라 다른 func 하도록 구현
 
@@ -100,14 +108,13 @@ class  IotPage02 : Fragment() {
         startActivity(intent)
     }
 
-    fun selectCallNum(lineName:Int = 0, stationName:String = "기본" ): String {
+    fun selectCallNum(lineName:Int = 0, stationName:String = "기본" ): Pair<Int, String> {
         val line1Seoul: Array<String> = arrayOf("서울역","시청","종각","종로3가","종로5가","동대문","동묘앞","신설동","제기동","청량리")
         val line3Seoul: Array<String> = arrayOf("지축","구파발","연신내","불광","녹번","홍제","무악재","독립문","경복궁","안국","종로3가","을지로3가","충무로","동대입구","약수","금호","옥수","압구정","신사","잠원","고속터미널","교대","남부터미널","양재","매봉","도곡","대치","학여울","대청","일원","수서","가락시장","경찰병원","오금")
         val line4Seoul: Array<String> = arrayOf("당고개","상계","노원","창동","쌍문","수유","미아","미아삼거리","길음","성신여대입구","돈암","혜화","동대문","동대문운동장","충무로","명동","회현","남대문시장","서울역","숙대입구","삼각지","신용산","이촌","동작","총신대입구","이수","사당","남태령")
         //당고개 남태령
-        var resultStr:String = ""
 
-        resultStr = when {
+        var callnum = when {
             (lineName >= 5 && lineName <= 8) || lineName == 2 -> "15771234"
             lineName == 9 -> "0226560009"
             /*역 번호가 다를 때에*/
@@ -117,6 +124,9 @@ class  IotPage02 : Fragment() {
             lineName == 4 && (stationName in line4Seoul) -> "15771234"
             else -> "15447788"
         }
+
+
+        var resultStr:Pair<Int, String> = Pair(lineName, callnum)
 
         return resultStr
     }
